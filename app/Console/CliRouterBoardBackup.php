5796ea31ac236e46da6cc7a0657a4f68f73f6bac
heximcz <spacek@best-net.cz>
2016-02-13 00:29:30 +0100

    clear comments

--- app/Console/CliRouterBoardBackup.php
+++ app/Console/CliRouterBoardBackup.php
@@ -8,8 +8,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\ArrayInput;
-//use Src\RouterBoard\RouterBoardMod;
-//use Src\RouterBoard\SecureTools;
 use Src\Logger\OutputLogger;
 use Src\RouterBoard\RouterBoardBackup;
 
