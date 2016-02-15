b33a4cbd55c82de1cc70fee88c4569d2dcf467b2
heximcz <spacek@best-net.cz>
2016-02-15 01:44:28 +0100

    test tests

--- tests/routerboard-backup/ApplicationTest.php
+++ tests/routerboard-backup/ApplicationTest.php
@@ -12,20 +12,16 @@ use Dibi\Connection;
 
 class ApplicationTest extends \PHPUnit_Framework_TestCase {
 
-	protected $config;
-	
-	public function setUp() {
-		$myConfig = new GetYAMLConfig ();
-		$this->config = $myConfig->getConfigData ();
-	}
+	protected static $config;
+	protected static $db;
 	
 	public function testConfig() {
-		$this->assertTrue ( is_array ( $this->config ) );
+		$this->assertTrue ( is_array ( self::$config ) );
 	}
 	
 	public function testExecuteMod() {
 		$application = new Application ();
-		$application->add ( new CliRouterBoardModify ( $this->config ) );
+		$application->add ( new CliRouterBoardModify ( self::$config ) );
 		$command = $application->find ( 'rb:mod' );
 		$commandTester = new CommandTester ( $command );
 		$commandTester->execute ( array (
@@ -36,7 +32,7 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 	
 	public function testExecuteBackup() {
 		$application = new Application ();
-		$application->add ( new CliRouterBoardBackup ( $this->config ) );
+		$application->add ( new CliRouterBoardBackup ( self::$config ) );
 		$command = $application->find ( 'rb:backup' );
 		$commandTester = new CommandTester ( $command );
 		$commandTester->execute ( array (
@@ -47,7 +43,7 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 	
 	public function testExecuteList() {
 		$application = new Application ();
-		$application->add ( new CliRouterBoardList ( $this->config ) );
+		$application->add ( new CliRouterBoardList ( self::$config ) );
 		$command = $application->find ( 'rb:list' );
 		$commandTester = new CommandTester ( $command );
 		$commandTester->execute ( array (
@@ -57,9 +53,9 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 	}
 	
 	public function testRunCommandList() {
-		$this->setUpDatabase ();
+//		$this->setUpDatabase ();
 		$application = new Application ();
-		$application->add ( new CliRouterBoardList ( $this->config ) );
+		$application->add ( new CliRouterBoardList ( self::$config ) );
 		$command = $application->find ( 'rb:list' );
 		$commandTester = new CommandTester ( $command );
 		$commandTester->execute ( array (
@@ -70,21 +66,21 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testRunCommandsMod() {
-		$this->setUpDatabase ();
+//		$this->setUpDatabase ();
 		$application = new Application ();
-		$application->add ( new CliRouterBoardModify( $this->config ) );
+		$application->add ( new CliRouterBoardModify( self::$config ) );
 		$command = $application->find ( 'rb:mod' );
 		$commandTester = new CommandTester ( $command );
 		// update
 		$commandTester->execute ( array (
 				'action' => 'update',
-				'-i'  => ['192.168.1.1','192.168.1.2']
+				'-i'  => ['192.168.1.5','192.168.1.20']
 		) );
 		$this->assertRegExp ( '/../', $commandTester->getDisplay () );
 		// addnew
 		$commandTester->execute ( array (
 				'action' => 'addnew',
-				'-i'  => ['192.168.1.3']
+				'-i'  => ['192.168.1.21']
 		) );
 		$this->assertRegExp ( '/../', $commandTester->getDisplay () );
 		// delete
@@ -96,9 +92,9 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testRunCommandsBackup() {
-		$this->setUpDatabase ();
+//		$this->setUpDatabase ();
 		$application = new Application ();
-		$application->add ( new CliRouterBoardBackup( $this->config ) );
+		$application->add ( new CliRouterBoardBackup( self::$config ) );
 		$command = $application->find ( 'rb:backup' );
 		$commandTester = new CommandTester ( $command );
 		// backup one
@@ -116,25 +112,27 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 	}
 	
 	public function testIPAddr() {
-		$ip = new IPValidator( $this->config, new OutputLogger( new NullOutput() ) );
+		$ip = new IPValidator( self::$config, new OutputLogger( new NullOutput() ) );
 		$this->assertTrue( $ip->ipv4validator('192.168.1.254') );
 		$this->assertFalse( $ip->ipv4validator('192.168.1.256') );
 	}
 	
-	protected function setUpDatabase() {
-		$options = array (
-				'driver' => $this->config ['database'] ['driver'],
-				'host' => $this->config ['database'] ['host'],
-				'username' => $this->config ['database'] ['user'],
-				'database' => $this->config ['database'] ['database'],
-				'password' => $this->config ['database'] ['password'],
-				'charset' => $this->config ['database'] ['charset'],
-				'port' => $this->config ['database'] ['port'],
-				'persistent' => $this->config ['database'] ['persistent'],
-				'dsn' => $this->config ['database'] ['dsn'] 
+	public static function setUpBeforeClass() {
+		$myConfig = new GetYAMLConfig ();
+		self::$config = $myConfig->getConfigData ();
+		$config = array (
+				'driver' => self::$config ['database'] ['driver'],
+				'host' => self::$config ['database'] ['host'],
+				'username' => self::$config ['database'] ['user'],
+				'database' => self::$config ['database'] ['database'],
+				'password' => self::$config ['database'] ['password'],
+				'charset' => self::$config ['database'] ['charset'],
+				'port' => self::$config ['database'] ['port'],
+				'persistent' => self::$config ['database'] ['persistent'],
+				'dsn' => self::$config ['database'] ['dsn'] 
 		);
-		$connection = new Connection ( $options );
-		$connection->query ( "CREATE TABLE IF NOT EXISTS [routers] (
+		self::$db = new Connection ( $config );
+		self::$db->query ( "CREATE TABLE IF NOT EXISTS [routers] (
   					[id] int(11) NOT NULL AUTO_INCREMENT,
   					[addr] char(15) COLLATE utf8_bin NOT NULL,
   					[identity] varchar(255) COLLATE utf8_bin DEFAULT NULL,
@@ -144,12 +142,20 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 					PRIMARY KEY ([id])
 					) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 					" );
-		$args = [ 
-				'addr' => '192.168.1.1',
-				'identity' => 'RB-Test',
-				'created' => new \DateTime () 
-		];
-		$connection->query ( 'INSERT INTO [routers]', $args );
+		for ($i=1; $i<11; $i++) {
+			$args = [ 
+					'addr' => '192.168.1.' . $i,
+					'identity' => 'RB-Test' . $i,
+					'created' => new \DateTime () 
+			];
+			self::$db->query ( 'INSERT INTO [routers]', $args );
+		}
 	}
 
+	
+	public static function tearDownAfterClass()
+	{
+		self::$db->query ( "DROP TABLE [routers]" );
+	}
+	
 }
