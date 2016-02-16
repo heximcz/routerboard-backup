359d80d6f7648b9f29178707132e9c33b1f9f7cb
heximcz <spacek@best-net.cz>
2016-02-16 15:43:56 +0100

    sanity check

--- src/Routerboard/RouterBoardBackup.php
+++ src/Routerboard/RouterBoardBackup.php
@@ -10,11 +10,11 @@ class RouterBoardBackup extends AbstractRouterBoard implements IRouterBoardBacku
 	 * @see \Src\RouterBoard\IRouterBoardBackup::backupAllRouterBoards()
 	 */
 	public function backupAllRouterBoards() {
-		$db = new $this->config['database']['data-adapter']($this->config, $this->logger);
-		if ( $result = $db->getIP() ) {
-			$rb = new SSHConnector($this->config, $this->logger);
+		$dbconnect = new $this->config['database']['data-adapter']($this->config, $this->logger);
+		if ( $result = $dbconnect->getIP() ) {
+			$ssh = new SSHConnector($this->config, $this->logger);
 			foreach ($result as $data) {
-				$rb->getBackupFile($data['addr'], $data['identity']);
+				$ssh->getBackupFile($data['addr'], $data['identity']);
 			}
 			return;
 		}
@@ -26,12 +26,12 @@ class RouterBoardBackup extends AbstractRouterBoard implements IRouterBoardBacku
 	 * @see \Src\RouterBoard\IRouterBoardBackup::backupOneRouterBoard()
 	 */
 	public function backupOneRouterBoard(array $addr) {
-		$db = new $this->config['database']['data-adapter']($this->config, $this->logger);
-		$rb = new SSHConnector($this->config, $this->logger);
+		$dbconnect = new $this->config['database']['data-adapter']($this->config, $this->logger);
+		$ssh = new SSHConnector($this->config, $this->logger);
 		foreach ($addr as $ip) {
-			if ( $db->checkExistIP($ip) ) {
-				$data = $db->getOneIP($ip);
-				$rb->getBackupFile( $data[0]['addr'], $data[0]['identity'] );
+			if ( $dbconnect->checkExistIP($ip) ) {
+				$data = $dbconnect->getOneIP($ip);
+				$ssh->getBackupFile( $data[0]['addr'], $data[0]['identity'] );
 				continue;
 			}
 			$this->logger->log('IP addresses: ' . $ip . ' does not exist in the database! Add this IP address first.', $this->logger->setError() );
