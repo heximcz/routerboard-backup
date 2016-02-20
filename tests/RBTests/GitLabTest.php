e98d0a395af1afb8844b5dbeda5a2120e0cda01a
heximcz <spacek@best-net.cz>
2016-02-20 14:19:08 +0100

    aad gitlab test

---
+++ tests/RBTests/GitLabTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace RBTests;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use App\Console\CliRouterBoardGitLab;
+
+class GitLabTest extends RBCaseTest {
+
+	public function testExecuteGitLabBackup() {
+		$application = new Application ();
+		$application->add ( new CliRouterBoardGitLab ( self::$config ) );
+		$command = $application->find ( 'rb:gitlab' );
+		$commandTester = new CommandTester ( $command );
+		$commandTester->execute ( array (
+				'action' => $command->getName () 
+		) );
+		$this->assertRegExp ( '/.../', $commandTester->getDisplay () );
+	}
+
+	public function testRunCommandsGitLabBackupOne() {
+		$application = new Application ();
+		$application->add ( new CliRouterBoardGitLab( self::$config ) );
+		$command = $application->find ( 'rb:gitlab' );
+		$commandTester = new CommandTester ( $command );
+		// backup one
+		$commandTester->execute ( array (
+				'action' => 'backup',
+				'-i'  => ['192.168.1.7']
+		) );
+		$this->assertRegExp ( '/../', $commandTester->getDisplay () );
+	}
+
+	public function testRunCommandsGitLabBackupAll() {
+		$application = new Application ();
+		$application->add ( new CliRouterBoardGitLab( self::$config ) );
+		$command = $application->find ( 'rb:gitlab' );
+		$commandTester = new CommandTester ( $command );
+		// backupall
+		$commandTester->execute ( array (
+				'action' => 'backup',
+		) );
+		$this->assertRegExp ( '/../', $commandTester->getDisplay () );
+	
+	}
+	
+}
