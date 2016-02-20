ee020cc9f0206f7dce35ba04caf12c67942bf1cf
heximcz <spacek@best-net.cz>
2016-02-20 04:35:07 +0100

    sanity check

--- app/Config/GetYAMLConfig.php
+++ app/Config/GetYAMLConfig.php
@@ -1,43 +1,43 @@
 <?php
+
 namespace App\Config;
 
 use Symfony\Component\Yaml\Parser;
 use Exception;
 
 class GetYAMLConfig {
-	
+
 	private $defautConfigPath;
 	private $customConfigPath;
 	private $config;
-
+	
 	public function __construct() {
-		$this->defautConfigPath = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config.default.yml';
-		$this->customConfigPath = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config.yml';
-		$this->createConfig();
+		$this->defautConfigPath = dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config.default.yml';
+		$this->customConfigPath = dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config.yml';
+		$this->createConfig ();
 	}
 	
 	public function getConfigData() {
 		return $this->config;
 	}
 	
-	private function parseConfig( $configPath ) {
-		$yaml = new Parser();
-		return $yaml->parse( file_get_contents( $configPath ) );
+	private function parseConfig($configPath) {
+		$yaml = new Parser ();
+		return $yaml->parse ( file_get_contents ( $configPath ) );
 	}
 	
 	private function createConfig() {
-   		if ( file_exists( $this->defautConfigPath ) ) {
-       		$defconf = $this->parseConfig( $this->defautConfigPath );
-		    if ( file_exists( $this->customConfigPath ) ) {
-			    $customconf = $this->parseConfig( $this->customConfigPath );
-			    $this->config = array_replace_recursive( $defconf, $customconf );
-		    }
-		    else {
-		        $this->config = $defconf;
-		    }
-   		}
-   		else
-   		    throw new Exception( get_class($this) . ' FATAL ERROR: config.default.yml no exist!');
+		if ( file_exists ( $this->defautConfigPath ) ) {
+			$defconf = $this->parseConfig ( $this->defautConfigPath );
+			if ( file_exists ( $this->customConfigPath ) ) {
+				$customconf = $this->parseConfig ( $this->customConfigPath );
+				$this->config = array_replace_recursive ( $defconf, $customconf );
+				return;
+			}
+			$this->config = $defconf;
+			return;
+		}
+		throw new Exception ( get_class ( $this ) . ' FATAL ERROR: config.default.yml no exist!');
 	}
 	
 }
