7915e57f64c8185ea469b7dcf5faead2e5663425
heximcz <spacek@best-net.cz>
2016-02-20 17:59:08 +0100

    gitlab test

--- tests/RBTests/GitLabTest.php
+++ tests/RBTests/GitLabTest.php
@@ -8,14 +8,9 @@ use Gitlab\Exception\RuntimeException;
 
 class GitLabTest extends RBCaseTest {
 
-	/*
-	public function testEmpty()
-	{
-		$stack = array();
-		$this->assertEmpty($stack);
-
-	}
-*/	
+	/**
+	 * test only exception
+	 */
 	public function testExecuteGitLabBackup() {
 		try {
 			$application = new Application ();
@@ -32,31 +27,5 @@ class GitLabTest extends RBCaseTest {
 			$this->assertContains('resolve host', $e->getMessage());
 		}
 	}
-/*
-	public function testRunCommandsGitLabBackupOne() {
-		$application = new Application ();
-		$application->add ( new CliRouterBoardGitLab( self::$config ) );
-		$command = $application->find ( 'rb:gitlab' );
-		$commandTester = new CommandTester ( $command );
-		// backup one
-		$commandTester->execute ( array (
-				'action' => 'backup',
-				'-i'  => ['192.168.1.7']
-		) );
-		$this->assertRegExp ( '/../', $commandTester->getDisplay () );
-	}
 
-	public function testRunCommandsGitLabBackupAll() {
-		$application = new Application ();
-		$application->add ( new CliRouterBoardGitLab( self::$config ) );
-		$command = $application->find ( 'rb:gitlab' );
-		$commandTester = new CommandTester ( $command );
-		// backupall
-		$commandTester->execute ( array (
-				'action' => 'backup',
-		) );
-		$this->assertRegExp ( '/../', $commandTester->getDisplay () );
-	
-	}
-*/	
 }
