fc88c69cd58da05f49d7eef02264c35159fcdd37
heximcz <spacek@best-net.cz>
2016-02-20 17:33:54 +0100

    try exception

--- tests/RBTests/GitLabTest.php
+++ tests/RBTests/GitLabTest.php
@@ -4,27 +4,35 @@ namespace RBTests;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 use App\Console\CliRouterBoardGitLab;
+use Gitlab\Exception\RuntimeException;
 
 class GitLabTest extends RBCaseTest {
-	
+
+	/*
 	public function testEmpty()
 	{
 		$stack = array();
 		$this->assertEmpty($stack);
 
 	}
-/*
+*/	
 	public function testExecuteGitLabBackup() {
-		$application = new Application ();
-		$application->add ( new CliRouterBoardGitLab ( self::$config ) );
-		$command = $application->find ( 'rb:gitlab' );
-		$commandTester = new CommandTester ( $command );
-		$commandTester->execute ( array (
+		try {
+			$application = new Application ();
+			$application->add ( new CliRouterBoardGitLab ( self::$config ) );
+			$command = $application->find ( 'rb:gitlab' );
+			$commandTester = new CommandTester ( $command );
+			$commandTester->execute ( array (
+				'action' => 'backup',
 				'--help' => true
-		) );
-		$this->assertRegExp ( '/.../', $commandTester->getDisplay () );
+			) );
+			$this->assertRegExp ( '/../', $commandTester->getDisplay () );
+		} 
+		catch (RuntimeException $e) {
+			$this->assertContains('resolve host', $e->getMessage());
+		}
 	}
-
+/*
 	public function testRunCommandsGitLabBackupOne() {
 		$application = new Application ();
 		$application->add ( new CliRouterBoardGitLab( self::$config ) );
