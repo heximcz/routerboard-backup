7dd9ffe9e82552968776705054d17398e5dcabba
heximcz <spacek@best-net.cz>
2016-02-29 21:20:23 +0100

    better check

--- src/Routerboard/Validators/IPValidator.php
+++ src/Routerboard/Validators/IPValidator.php
@@ -10,12 +10,26 @@ class IPValidator extends AbstractRouterBoard {
 	 * @return boolean
 	 */
 	public function ipv4validator($addr) {
-		$addr = gethostbyname( $addr );
-		if (filter_var ( $addr, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) === false) {
-			$this->logger->log ( "IPv4 address is not valid: '" . $addr . "', sorry.", $this->logger->setError() );
+		if ( $this->ifDomainName($addr))
+			$addr = gethostbyname( $addr );
+		if ( filter_var ( $addr, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) === false ) {
+			$this->logger->log ( "IPv4 address (domain) '" . $addr . "' is not valid or not exist!", $this->logger->setError() );
 			return false;
 		}
 		return true;
+	}
+	
+	/**
+	 * Check if a string represents domain name
+	 * @param string $domain
+	 * @return boolean
+	 */
+	public function ifDomainName($domain) {
+		$validHostnameRegex = "/^[a-zA-Z0-9.\-]{2,256}\.[a-z]{2,6}$/";
+		if ( preg_match($validHostnameRegex, $domain) ) {
+			return true;
+		}
+		return false;
 	}
 
 }
