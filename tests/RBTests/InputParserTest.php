2d46c6506bbff327fb377679220b1106d7db12a3
heximcz <spacek@best-net.cz>
2016-03-02 23:29:05 +0100

    fixed InputParserTest

--- tests/RBTests/InputParserTest.php
+++ tests/RBTests/InputParserTest.php
@@ -7,14 +7,25 @@ use Symfony\Component\Console\Output\NullOutput;
 
 class InputParserTest extends RBCaseTest {
 	
-	public function testInputParser() {
+	public function testInputParserNormal() {
 		$input = array(
 				0 => '10.17.254.254:2345',
 				1 => '10.10.10.10',
 				2 => '192.168.1.1:2575'
 		);
 		$inputParser = new InputParser( self::$config, new OutputLogger( new NullOutput() ), $input );
-		$this->assertTrue( $inputParser->getAddr() );
+		$this->assertArrayHasKey( 'addr', $inputParser->getAddr() );
+		$this->assertArrayHasKey( 'port', $inputParser->getAddr() );
+	}
+
+	public function testInputParserError() {
+		$input = array(
+				0 => '10.17.254.300',
+				1 => '10.10.10.300',
+				2 => '192.168.1.300:2575'
+		);
+		$inputParser = new InputParser( self::$config, new OutputLogger( new NullOutput() ), $input );
+		$this->$this->assertFalse( $inputParser->getAddr() );
 	}
 
 }
