254942a26e981ad58b86847b5dbec86d143a1330
heximcz <spacek@best-net.cz>
2016-03-02 22:15:02 +0100

    fixed independent db adapter

--- src/Routerboard/RouterBoardList.php
+++ src/Routerboard/RouterBoardList.php
@@ -2,15 +2,13 @@
 
 namespace Src\RouterBoard;
 
-use Src\Adapters\RouterBoardDBAdapter;
-
 class RouterBoardList extends AbstractRouterBoard {
 	
 	/**
 	 * Print all info about routers from backup list
 	 */
 	public function printAllRouterBoards() {
-		$dbconnect = new RouterBoardDBAdapter( $this->config, $this->logger );
+		$dbconnect = new $this->config['database']['data-adapter']($this->config, $this->logger);
 		if ( $result = $dbconnect->getIP() ) {
 			foreach ($result as $data) {
 				$this->logger->log( $data['identity'] . ' - ' . $data['addr'], $this->logger->setNotice() );
