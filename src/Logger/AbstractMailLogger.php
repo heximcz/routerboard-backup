df5c98744e66fdf67a4456188bb107d95bc06fdc
hexim <spacek@best-net.cz>
2016-11-09 21:28:31 +0100

    fix error message

--- src/Logger/AbstractMailLogger.php
+++ src/Logger/AbstractMailLogger.php
@@ -30,7 +30,7 @@ abstract class AbstractMailLogger
         $mail->FromName = $host;
         $mail->addAddress($emailTo);
         $mail->isHTML(true);
-        $mail->Subject = 'Error on DNS Resolver -> ' . $host;
+        $mail->Subject = 'Error -> ' . $host;
         $mail->Body = str_replace("\n", '<br />', $this->mailBody);
         $mail->AltBody = $this->mailBody;
         return ($mail->send() ? true : false);
