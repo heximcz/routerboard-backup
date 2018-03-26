d729a784712acdb9099232266e34199e78b6cbf8
hexim <hexim@hexim.cz>
2018-03-27 01:21:30 +0200

    fix print right version

--- routerboard-backup.php
+++ routerboard-backup.php
@@ -1,6 +1,5 @@
 <?php
 use Symfony\Component\Console\Application;
-use Symfony\Component\Filesystem\Exception\IOException;
 use App\Config\GetYAMLConfig;
 use App\Console\CliRouterBoardModify;
 use App\Console\CliRouterBoardBackup;
@@ -13,7 +12,7 @@ require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'vendor/autoload.php';
 try {
 	$myConfig = new GetYAMLConfig();
 	$config   = $myConfig->getConfigData();
-	$application = new Application( "Mikrotik RouterBoard tools:","1.0.1" );
+	$application = new Application( "Mikrotik RouterBoard Backup:","1.0.2" );
 	$application->add( new CliRouterBoardModify( $config ) );
 	$application->add( new CliRouterBoardBackup( $config ) );
 	$application->add( new CliRouterBoardList( $config ) );
@@ -21,7 +20,5 @@ try {
     $application->add( new CliRouterBoardDecode( $config ) );
 	$application->run();
 } catch (Exception $e) {
-	echo 'Caught exception: ',  $e->getMessage(), "\n";
-} catch (IOException $e) {
-	echo 'Caught exception: ',  $e->getMessage(), "\n";
+    echo 'Caught exception: ', $e->getMessage(), "\n";
 }
